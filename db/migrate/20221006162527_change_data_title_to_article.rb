87ff74e94cbac5c2561a4e428f3c9fca03782c84
EC2 Default User <ec2-user@ip-172-31-15-78.ec2.internal>
2022-10-11 08:35:40 +0000

    ファイル数の削減

--- db/migrate/20221006162527_change_data_title_to_article.rb
+++
@@ -1,8 +0,0 @@
-class ChangeDataTitleToArticle < ActiveRecord::Migration[6.1]
-  def change
-    change_column :users, :start , :date
-    change_column :users, :fin, :date
-    change_column :users, :all,:boolean
-    change_column :users, :memo,:string
-  end
-end
