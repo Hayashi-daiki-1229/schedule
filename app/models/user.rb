ee48de9e34d666c5453f8ca3fe5f28717d79e1fb
EC2 Default User <ec2-user@ip-172-31-15-78.ec2.internal>
2022-10-14 17:56:43 +0000

    バリデーションの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,13 @@
 class User < ApplicationRecord
+  validates :title,presence:true
+  validates :start, presence: true
+  validates :fin, presence: true
+  validates :all, {acceptance: true}
+  validates :memo, length: { in: 10..30 }
+  validate :start_end_check
+
+  def start_end_check
+    errors.add(:fin, "は開始日より前の日付は登録できません。") unless
+    self.start < self.fin
+  end
 end
